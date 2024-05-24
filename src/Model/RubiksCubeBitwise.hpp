5214b16ba9b03a508aa8eebb53777e2b0624bc9a
RobinJehn <robin.jehn@gmx.de>
2024-05-24 19:53:04 +0100

    feat: Implement RubiksCubeBitwise class and CubeViewer
    Implemented the RubiksCubeBitwise class including bitwise representation
    of the Rubik's cube and CubeViewer class to visualize the cube matrix in
    the terminal.

---
+++ src/Model/RubiksCubeBitwise.hpp
@@ -0,0 +1,138 @@
+#pragma once
+#include "RubiksCube.hpp"
+#include <array>
+#include <map>
+
+namespace cube {
+
+class RubiksCubeBitwise : public RubiksCube {
+private:
+  typedef RubiksCube::MOVE MOVE;
+  typedef RubiksCube::FACE FACE;
+  typedef RubiksCube::COLOR COLOR;
+
+  enum class ROTATION : uint8_t {
+    CLOCKWISE,
+    CLOCKWISE2,
+    COUNTERCLOCKWISE,
+  };
+
+  enum class FACE_EDGE : uint8_t {
+    UT, // Up Top
+    UR, // Up Right
+    UB, // Up Bottom
+    UL, // Up Left
+
+    LT, // Left Top
+    LR, // Left Right
+    LB, // Left Bottom
+    LL, // Left Left
+
+    FT, // Front Top
+    FR, // Front Right
+    FB, // Front Bottom
+    FL, // Front Left
+
+    RT, // Right Top
+    RR, // Right Right
+    RB, // Right Bottom
+    RL, // Right Left
+
+    DT, // Down Top
+    DR, // Down Right
+    DB, // Down Bottom
+    DL, // Down Left
+
+    BT, // Back Top
+    BR, // Back Right
+    BB, // Back Bottom
+    BL, // Back Left
+  };
+public:
+  COLOR cube[6][8];
+private:
+  /**
+   * @brief Rotates the face of the cube according to the rotation. Ignores the
+   * edges of the face.
+   *
+   * @param f Face to rotate.
+   * @param rotation Rotation to apply.
+   */
+  void rotateFace(FACE f, ROTATION rotation);
+
+  /**
+   * @brief Rotates the edges of the face of the cube according to the rotation.
+   * Ignores the face itself.
+   *
+   * @param f Face to rotate.
+   * @param rotation Rotation to apply.
+   */
+  void rotateEdges(FACE f, ROTATION rotation);
+
+  /**
+   * @brief Applies the rotation to the face and edges of the face.
+   *
+   * @param f Face to rotate.
+   * @param rotation Rotation to apply.
+   */
+  void rotate(FACE f, ROTATION rotation);
+
+  // Edge mapping
+  std::map<FACE, std::array<FACE_EDGE, 4>> edgeMap = {
+      {FACE::UP, {FACE_EDGE::BB, FACE_EDGE::RT, FACE_EDGE::FT, FACE_EDGE::LT}},
+      {FACE::DOWN,
+       {FACE_EDGE::LB, FACE_EDGE::FB, FACE_EDGE::RB, FACE_EDGE::BT}},
+      {FACE::LEFT,
+       {FACE_EDGE::UL, FACE_EDGE::FL, FACE_EDGE::DL, FACE_EDGE::BL}},
+      {FACE::RIGHT,
+       {FACE_EDGE::UR, FACE_EDGE::BR, FACE_EDGE::DR, FACE_EDGE::FL}},
+      {FACE::FRONT,
+       {FACE_EDGE::LR, FACE_EDGE::UB, FACE_EDGE::RL, FACE_EDGE::DT}},
+      {FACE::BACK,
+       {FACE_EDGE::DB, FACE_EDGE::RR, FACE_EDGE::UT, FACE_EDGE::LL}}};
+
+public:
+  RubiksCubeBitwise();
+
+  RubiksCube &u() override;
+  RubiksCube &uPrime() override;
+  RubiksCube &u2() override;
+
+  RubiksCube &d() override;
+  RubiksCube &dPrime() override;
+  RubiksCube &d2() override;
+
+  RubiksCube &l() override;
+  RubiksCube &lPrime() override;
+  RubiksCube &l2() override;
+
+  RubiksCube &r() override;
+  RubiksCube &rPrime() override;
+  RubiksCube &r2() override;
+
+  RubiksCube &f() override;
+  RubiksCube &fPrime() override;
+  RubiksCube &f2() override;
+
+  RubiksCube &b() override;
+  RubiksCube &bPrime() override;
+  RubiksCube &b2() override;
+
+  RubiksCube &m() override;
+  RubiksCube &mPrime() override;
+  RubiksCube &m2() override;
+
+  RubiksCube &e() override;
+  RubiksCube &ePrime() override;
+  RubiksCube &e2() override;
+
+  RubiksCube &s() override;
+  RubiksCube &sPrime() override;
+  RubiksCube &s2() override;
+
+  RubiksCube::COLOR getColor(FACE face, unsigned row,
+                             unsigned col) const override;
+  bool isSolved() const override;
+};
+
+} // namespace cube
---
+++ src/View/TerminalView.cpp
@@ -0,0 +1,58 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+class CubeViewer {
+public:
+  CubeViewer(const std::vector<std::vector<int>> &matrix) {
+    if (matrix.size() != 6 || !std::all_of(matrix.begin(), matrix.end(),
+                                           [](const std::vector<int> &row) {
+                                             return row.size() == 8;
+                                           })) {
+      throw std::invalid_argument("Matrix must be 6x8.");
+    }
+    this->matrix = matrix;
+  }
+
+  void display() const {
+    const std::vector<int> &top = matrix[0];
+
+    std::cout << "      " << top[0] << ' ' << top[1] << ' ' << top[2] << '\n';
+    std::cout << "      " << top[7] << "   " << top[3] << '\n';
+    std::cout << "      " << top[6] << ' ' << top[5] << ' ' << top[4] << '\n';
+
+    for (int j = 0; j < 9; ++j) {
+      int face = (int)j / 3 + 1;
+      int idx = j % 3;
+      std::cout << matrix[face][idx] << ' ';
+    }
+
+    std::cout << "\n"
+              << matrix[1][7] << "   " << matrix[1][3] << " " << matrix[2][7]
+              << "   " << matrix[2][3] << " " << matrix[3][7] << "   "
+              << matrix[3][3] << '\n';
+
+    for (int j = 0; j < 9; ++j) {
+      int face = (int)j / 3 + 1;
+      int idx = 6 - j % 3;
+      std::cout << matrix[face][idx] << ' ';
+    }
+
+    const std::vector<int> &b_bottom = matrix[4];
+    std::cout << "\n      " << b_bottom[0] << ' ' << b_bottom[1] << ' '
+              << b_bottom[2] << '\n';
+    std::cout << "      " << b_bottom[7] << "   " << b_bottom[3] << '\n';
+    std::cout << "      " << b_bottom[6] << ' ' << b_bottom[5] << ' '
+              << b_bottom[4] << '\n';
+
+    const std::vector<int> &bottom = matrix[5];
+    std::cout << "      " << bottom[0] << ' ' << bottom[1] << ' ' << bottom[2]
+              << '\n';
+    std::cout << "      " << bottom[7] << "   " << bottom[3] << '\n';
+    std::cout << "      " << bottom[6] << ' ' << bottom[5] << ' ' << bottom[4]
+              << '\n';
+  }
+
+private:
+  std::vector<std::vector<int>> matrix;
+};
